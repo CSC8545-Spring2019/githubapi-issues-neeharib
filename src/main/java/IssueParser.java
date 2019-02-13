1b228c675d3135c2518595150ef3035846444a23
neeharib <neeharika.royal@gmail.com>
2019-02-13 01:43:34 -0500

    Parsing Json With Gson.Jar and Modifying parse function

--- src/main/java/IssueParser.java
+++ src/main/java/IssueParser.java
@@ -1,9 +1,23 @@
 package main.java;
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 public class IssueParser {
    public List<Issue> parseIssues(String s) {
-        return new ArrayList<>();
+       Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+               .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+               .create();
+              List<Issue> issuelist = new ArrayList<Issue>();
+              Type collectionType = new TypeToken<List<Issue>>(){}.getType();
+              issuelist = gson.fromJson(s, collectionType);
+              
+        return  issuelist;
     }
 }
